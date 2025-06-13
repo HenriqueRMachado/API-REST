90dafb62fb8950280d1c8a12bdc31198cd97f146
HenriqueRMachado <henrique.machado25@univille.br>
2025-06-12 22:21:47 -0300

    Update trabalho.js

--- trabalho.js
+++ trabalho.js
@@ -6,9 +6,9 @@ const port = 3000 // define a porta do localhost
 //Indica ao servior que iremos trabalhar com JSON 
 app.use(express.json());
 
-/*
-    Simula um banco de dados
-*/ 
+// /*
+//     Simula um banco de dados
+// */ 
 
 let cursos = [
     { id: 1, name: "Engenharia de Software", CargaHoraria: "4000", Universidade: "Univille"},
@@ -19,36 +19,48 @@ let cursos = [
 ];
 
 let alunos = [
-    { id: 1, name: "Lucas", email: "lucas@gmail.com", matricula: 2983428374, telefone: 4002-8922},
-    { id: 2, name: "Henrique", email: "henrique@gmail.com", matricula: 234234267, telefone: 4002-8922},
-    { id: 3, name: "Marcelo", email: "marcelo@gmail.com", matricula: 3284237843, telefone: 4002-8922},
-    { id: 4, name: "Camilli", email: "camilli@gmail.com", matricula: 7394237642, telefone: 4002-8922},
-    { id: 5, name: "Gustavo", email: "gustavo@gmail.com", matricula: 9834826344, telefone: 4002-8922},
+    { id: 1, name: "Lucas", email: "lucas@gmail.com", matricula: "2983428374", telefone: "4002-8922"},
+    { id: 2, name: "Henrique", email: "henrique@gmail.com", matricula: "234234267", telefone: "4002-8922"},
+    { id: 3, name: "Marcelo", email: "marcelo@gmail.com", matricula: "3284237843", telefone: "4002-8922"},
+    { id: 4, name: "Camilli", email: "camilli@gmail.com", matricula: "7394237642", telefone: "4002-8922"},
+    { id: 5, name: "Gustavo", email: "gustavo@gmail.com", matricula: "9834826344", telefone: "4002-8922"},
 ];
 
 let professores = [
-    {id: 1, nome: "Roberson", periodo: "1 semestre", disciplina: "Matematica Discreta"},
-    {id: 2, nome: "Andressa", periodo: "3 semestre", disciplina: "Desenvolvimento Web"},
-    {id: 3, nome: "Andrei", periodo: "2 e 3 semestre", disciplina: "Banco de Dados"},
-    {id: 4, nome: "Lucia", periodo: "5 e 6 semestre", disciplina: "Rede de Computadores"},
-    {id: 4, nome: "Marcos", periodo: "3 e 4 semestre", disciplina: "Algoritmo"},
+    {id: 1, name: "Roberson", periodo: "1 semestre", disciplina: "Matematica Discreta"},
+    {id: 2, name: "Andressa", periodo: "3 semestre", disciplina: "Desenvolvimento Web"},
+    {id: 3, name: "Andrei", periodo: "2 e 3 semestre", disciplina: "Banco de Dados"},
+    {id: 4, name: "Lucia", periodo: "5 e 6 semestre", disciplina: "Rede de Computadores"},
+    {id: 5, name: "Marcos", periodo: "3 e 4 semestre", disciplina: "Algoritmo"},
 ];
 
 
 
+
+
+// GPS DE ITENS DO ENDPOINT DE PROFESSORES
+
+function linkprofessores(prof) {
+    return {
+        self: { href: `/professores/${prof.id}`},
+        update: { href: `/professores/${prof.id}`, method: "PUT"},
+        delete: { href: `/professores/${prof.id}`, method: "DELETE"},
+        post: {href: `/professores/${prof.id}`, method: "POST"}
+    }
+};
 
 // GET ALL
 
 app.get('/professores', (req, res) => {
     const response = professores.map(prof => ({
         ...prof,
-        link: generationItemLink(prof),
+        link: linkprofessores(prof),
     }));
     res.status(200).json(response);
 });
 
 
-// VERBO get do ep professores
+// VERBO get do ep professores -- PEGA PELO ID ESPECIFICADO NA URL
 app.get('/professores/:id', (req, res) => {
     const id = parseInt(req.params.id);
     const prof = professores.find(prof => prof.id === id);
@@ -57,7 +69,7 @@ app.get('/professores/:id', (req, res) => {
 
         const response = professores.map(prof => ({
         ...prof,
-        link: generationItemLink(prof),
+        link: linkprofessores(prof),
     }));
 
         res.status(200).json(response[id - 1]);
@@ -67,34 +79,84 @@ app.get('/professores/:id', (req, res) => {
 });
 
 
-function generationItemLink(prof) {
-    return {
-        self: { href: `/professores/${prof.id}`},
-        update: { href: `/professores/${prof.id}`, method: "PUT"},
-        delete: { href: `/professores/${prof.id}`, method: "DELETE"},
-        post: {href: `/professores${prof.id}`, method: "POST"}
-    }
-}
+app.post('/professores', (req, res) => {
 
-// GPS DE ITENS DO ENDPOINT DE PROFESSORES
-function generationItemLink(prof) {
-    return {
-        self: { href: `/professores/${prof.id}`},
-        update: { href: `/professores/${prof.id}`, method: "PUT"},
-        delete: { href: `/professores/${prof.id}`, method: "DELETE"},
-        post: {href: `/professores${prof.id}`, method: "POST"}
+    //Validação para garantir que o campo 'name' não seja vazio
+    if (!req.body.name) {
+        return res.status(422).json({ message: "O campo 'name' não pode ser vazio." });
     }
-}
+    else if (!req.body.periodo) {
+        return res.status(422).json({ message: "O campo 'periodo' não pode ser vazio." });
+    }
+    else if (!req.body.disciplina ) {
+        return res.status(422).json({ message: "O campo 'disciplina' não pode ser vazio." });
+    }
+    //os arrays tem uma propriedade chamada length... essa propriedade calcula o tamanho
+    //do meu vetor e retorna ele em formato de inteiro...
+    const newItem = { id: cursos.length + 1, ...req.body}
+    //push insere um novo item no vetor...
+    professores.push(newItem);
+    res.status(201).json(newItem);
+});
+
+
+//Endpoint para apagar um item, método DELETE
+app.delete('/professores/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const index = professores.findIndex(prof => prof.id === id);
+    if(index !== -1) {
+        //desafio remover o item do array
+        professores.splice(index, 1);
+        res.status(200).json({mensage: "Item removido!"});
+    } else {
+        res.status(404).json({mensage: "Item não encontrado"});
+    }
+});
+
+//Endpoint para atualizar itens da lista, método PUT
+
+//RUSSIA = PUTin
+
+app.put('/professores/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const index = professores.findIndex(prof => prof.id === id);
+    
+    // 8 - Validação para garantir que o campo 'name' não seja vazio
+    if (!req.body.name || req.body.name.trim() === "") {
+        return res.status(422).json({ message: "O campo 'name' não pode ser vazio." });
+    }
+
+    if (index !== -1) {
+        professores[index] = {id, ...req.body}
+        res.status(200).json(professores[index]);
+    } else {
+        res.status(404).json({ message: "Item não encontrado!"});
+    }
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 //Mapa de itens  
-function generationItemLink(curso) {
+function linkcurso(curso) {
     return {
         self: { href: `/curso/${curso.id}`},
         update: { href: `/curso/${curso.id}`, method: "PUT"},
         delete: { href: `/curso/${curso.id}`, method: "DELETE"},
-        post: {href: `/curso${curso.id}`, method: "POST"}
+        post: {href: `/curso/${curso.id}`, method: "POST"}
     }
-}
+};
 
 // ENDPOINT DO BANCO DE DADOS CURSOS
 
@@ -102,7 +164,7 @@ function generationItemLink(curso) {
 app.get('/curso', (req, res) => {
     const response = cursos.map(curso => ({
         ...curso,
-        link: generationItemLink(curso),
+        link: linkcurso(curso),
     }));
     res.status(200).json(response);
 });
@@ -120,7 +182,7 @@ app.get('/curso/:id', (req, res) => {
 
         const response = cursos.map(curso => ({
         ...curso,
-        link: generationItemLink(curso),
+        link: linkcurso(curso),
     }));
 
         res.status(200).json(response[id - 1]);
@@ -176,7 +238,7 @@ app.put('/curso/:id', (req, res) => {
 
     if (index !== -1) {
         cursos[index] = {id, ...req.body}
-        res.status(200).json(alunos[index]);
+        res.status(200).json(cursos[index]);
     } else {
         res.status(404).json({ message: "Item não encontrado!"});
     }
@@ -191,19 +253,19 @@ app.put('/curso/:id', (req, res) => {
 
 
 //Mapa de itens
-function generationItemLink(aluno) {
+function linkaluno(aluno) {
     return {
         self: { href: `/aluno/${aluno.id}`},
         update: { href: `/aluno/${aluno.id}`, method: "PUT"},
         delete: { href: `/aluno/${aluno.id}`, method: "DELETE"}
     }
-}
+};
 
 //Endpoint para buscar todos os itens da lista, método GET
 app.get('/aluno', (req, res) => {
     const response = alunos.map(aluno => ({
         ...aluno,
-        link: generationItemLink(aluno),
+        link: linkaluno(aluno),
     }));
     res.status(200).json(response);
 });
@@ -220,7 +282,7 @@ app.get('/aluno/:id', (req, res) => {
 
         const response = alunos.map(aluno => ({
         ...aluno,
-        link: generationItemLink(aluno),
+        link: linkaluno(aluno),
     }));
 
         res.status(200).json(response[id - 1]);
@@ -276,4 +338,4 @@ app.put('/aluno/:id', (req, res) => {
 
 app.listen(port, () => {
     console.log(`O servidor está rodando em http://localhost:${port}`);
-})
+});
